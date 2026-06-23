254ce9054703980552179a8e4f38e2da19c1eb7b
LordGabo <gmtzgabito@gmail.com>
2026-06-23 17:39:16 -0600

    feat(security): middleware para logs de desarrollo
    nuevo middleware que sera el firewall y leer actividades de la web por medio del log de desarrollo

---
+++ app/Http/Middleware/DesarrolloLogMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DesarrolloLogMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(\Illuminate\Http\Request $request, \Closure $next)
+{
+    // Registrar la actividad entrante antes de que la página cargue
+    \Illuminate\Support\Facades\Log::channel('desarrollo')->debug('[WEB_ACTIVITY] Petición entrante HTTP', [
+        'metodo' => $request->method(),
+        'url' => $request->fullUrl(),
+        'usuario_autenticado' => auth()->check() ? auth()->user()->email : 'Invitado',
+        'ip' => $request->ip(),
+    ]);
+
+    return $next($request);
+}
+}
